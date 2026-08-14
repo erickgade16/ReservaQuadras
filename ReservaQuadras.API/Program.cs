b4b03a72ad8363dc6f39bf6c398da2dd07b5665f
erickgade16 <erickgade16@gmail.com>
2026-08-14 09:13:58 -0300

    feat: implementando pra nao dar erro no CORS

--- ReservaQuadras.API/Program.cs
+++ ReservaQuadras.API/Program.cs
@@ -29,6 +29,17 @@ builder.Services.AddDbContext<ReservaQuadrasContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("ReactPolicy", policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -37,6 +48,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("ReactPolicy");
 
 app.UseHttpsRedirection();
 
